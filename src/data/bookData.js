f6e48bda5637969a44416714d73e6316cd7a3857
Daviti <62611237+davitigoch@users.noreply.github.com>
2025-12-24 15:32:59 -0500

    Add book data for multiple children's tales

---
+++ src/data/bookData.js
@@ -0,0 +1,32 @@
+const books = {
+  tale1: {
+    book: "tale1",
+    picture: "tale1.jpg",
+    name: "hide and seek",
+    anotation:
+      "Abe was a real Service Dog who dedicated his life assisting BJ, a good family friend. Service Dogs are smart, well trained, well behaved, dedicated, and committed to ensuring their master is safe. This book is intended to bring an awareness of their importance to early readers.",
+  },
+  tale2: {
+    book: "tale2",
+    picture: "tale2.jpg",
+    name: "The Lunker",
+    anotation:
+      "Let's go fishing and a rhyming in this free illustrated children's book. Monday through Saturday was dad's to take but come Sunday, the lad caught a lunker. And boy! Wasn't he absolutely delighted?",
+  },
+  tale3: {
+    book: "tale3",
+    picture: "tale3.jpg",
+    name: "Hammy The Hamster",
+    anotation:
+      "He's got swag. He wears a tux. He's a hipster hamster who likes to live free. Join Hammy in his South American home as you read this free story book for kids on your next read aloud day.",
+  },
+  tale4: {
+    book: "tale4",
+    picture: "tale4.jpg",
+    name: "The Tooth Fairies",
+    anotation:
+      "A wonderful story that weaves its magic in the child's imagination, even while imparting an invaluable lesson in oral health and hygiene. This is a story that the children will return to time and again for a fun bedtime read.",
+  },
+};
+
+export default books;
