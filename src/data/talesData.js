93a52ce3661e4196e0e62cd2a55fe3faa2a62c6b
Daviti <62611237+davitigoch@users.noreply.github.com>
2025-12-24 15:33:14 -0500

    Add files via upload

---
+++ src/data/talesData.js
@@ -0,0 +1,260 @@
+const tales = {
+  tale1: [
+    {
+      text: `Hi, I am Abe and I am a Service Dog. \n      
+      When I was a puppy, my owner told me I would be a Service Dog for a person who could not see. I didn’t know what that meant,but I knew it was something special.\n
+When I got a little older, I was sent away to school.\n
+My sister was sent to another school\n
+to learn how to help people with Autism.\n
+To be their friend and help them get around.`,
+      image: "tale1_1.png",
+    },
+    {
+      text: `One of my other brothers was sent to a special school so he could learn how to warn people that they were going to get sick before they did.\n
+My other brothers and sisters became good friends with people in good families.\n
+Not every dog can become a Service Dog.\n
+School was hard.\n
+I had to learn many things just like you.\n
+I had to learn to stop and look before crossing the street.\n
+And learn what a red, yellow, and green directional light meant and what I should do.`,
+      image: "tale1_2.png",
+    },
+    {
+      text: `I was taught how to guide a person to go left, right, to step up and down, to stop, to move forward a little, to back up, and much more.\n
+I had to learn where I was and how to get back, even in strange areas.\n
+I had to learn how to behave in a restaurant and never, ever, beg for food. That was hard.\n
+I had to learn that I could be a plain old playful dog as long as my halter was off.\n
+But once it was on… I was working.\n
+I became the eyes of my master and their safety and how they got around was up to me.`,
+      image: "tale1_3.png",
+    },
+    {
+      text: `After a long time in school, I graduated and my trainer introduced me to BJ.\n
+The person who would become my master. We were both excited.\n
+We trained together for several weeks and became very good friends.\n
+She was really nice and we always spent fun-time together once she took my halter off.\n
+One day the trainer said we were ready and I was off to a new home with my master and friend.\n
+We shared many years together and had many wonderful experiences.\n
+We went shopping, visited friends, ate in fine restaurants, and traveled across the country.\n
+I took care of BJ.\n
+I knew when she didn’t feel right and could tell if she was happy or unhappy, sad, grumpy, or whatever.\n
+I would always try to make things better for her.\n`,
+      image: "tale1_4.png",
+    },
+    {
+      text: `BJ took care of me.\n
+She knew when I didn’t feel right and could tell if I were happy or unhappy, sad, grumpy, or whatever.\n
+She would always try to make things better for me.\n
+A long time has passed and now I can’t help my friend any more.\n
+BJ is off to school again and training with a new Service Dog.\n
+I will meet him tomorrow and help him understand his new master.\n
+Then I become like my other brothers and sisters.\n
+I become a good friend for people in a good family.\n
+You really couldn’t ask for more.`,
+      image: "tale1_5.png",
+    },
+  ],
+  tale2: [
+    {
+      text: `On Monday I went fishing with my dad.\n      
+      He caught a big one, I was sad.\n
+On Tuesday, we fished  a different spot.\n
+He caught another and I did not.\n
+to learn how to help people with Autism.`,
+      image: "lunker_1.png",
+    },
+    {
+      text: `On Wednesday, we fished a different lake.\n
+The fish were  there for him to take.\n
+Thursday came and we hit the river.\n
+He caught  five, I got a sliver.\n
+On Friday I told him, I needed a rest.\n
+He said, “Let us go, and give it your best.”`,
+      image: "lunker_2.png",
+    },
+    {
+      text: `Saturday came and he caught more.\n
+I tried so hard, my arms were sore.\n
+Sunday morning we were there very fast.\n
+I baited my hook and made a cast.\n
+A tug on my line, my reel was screaming.\n
+I caught a fish, my eyes were gleaming.`,
+      image: "lunker_3.png",
+    },
+    {
+      text: `When I got it to the boat.\n
+We were amazed to see.\n
+The biggest fish in the lake was caught by me!`,
+      image: "lunker_4.png",
+    },
+  ],
+  tale3: [
+    {
+      text: `I am Hammy the Hamster.\n      
+      I live with my mother Henrietta, my father Harry,\n
+We live in South America\n
+and you can’t miss our home.\n
+It is the burrow under\n
+the trees that look like the letter H.`,
+      image: "hammy_1.png",
+    },
+    {
+      text: `When we are out during the day,\n
+we are very careful so we aren’t caught.\n
+We might get trapped and become someone’s pet.\n
+So we are mostly out after dark.\n
+We like to eat seeds and fruit.\n
+and can carry it back home in our pouches.\n
+Our cheeks have built in shopping bags.`,
+      image: "hammy_2.png",
+    },
+    {
+      text: `Just like you, we like to play and have fun.\n
+We have fun running into the light\n
+and chasing our shadows on the leaves.\n
+Last night Hachi and I found this thing.\n
+When you pushed a button, it lit up like the sun.\n
+We like to climb things and explore.`,
+      image: "hammy_3.png",
+    },
+    {
+      text: `Our cousins Lester, Larry, and Lorain live in the city.\n
+They are someone’s pet.\n
+They sent us this picture and said\n 
+they had a lot of fun but missed playing with us.`,
+      image: "hammy_4.png",
+    },
+    {
+      text: `Lester said he liked people.\n
+He always had food and water\n
+and lots of toys to play with.\n
+He even likes to take a ride\n
+in the pocket of his people friend, Billy.`,
+      image: "hammy_5.png",
+    },
+    {
+      text: `I might be your pet some day.\n
+If I ever become your pet,\n
+I hope you take good care of me\n
+because I will do all I can to be your friend.\n
+Adios Amigos.`,
+      image: "hammy_6.png",
+    },
+  ],
+  tale4: [
+    {
+      text: `Deep in the New Forest where\n 
+no big people ever went there was a tiny little\n      
+magical village called the Ordon Gardens.\n
+Here, three little tooth fairies named Incisor, Canine and Molar\n
+were busy at work in their factory.\n
+The three tooth fairies were all named after types of teeth.`,
+      image: "fairy_1.png",
+    },
+    {
+      text: `Incisor was tall and skinny with short pink hair,\n
+Canine was also tall with blue hair\n
+and Molar was the short, wise one with curly, silver locks.\n
+All day long the busy little fairies cleaned\n
+and shined the children’s milk teeth they\n
+had collected during the night.`,
+      image: "fairy_2.png",
+    },
+    {
+      text: `The milk teeth were used to make very\n
+useful things in the Ordon Gardens like roads\n
+in the village and furniture in the homes for the little fairies.\n
+One day Incisor realised that the teeth coming\n
+into the cleaning factory were not\n
+as strong as they used be.\n
+“Oh dear!” said Incisor “These teeth are fragile\n
+and brittle, with holes and black spots.\n
+They crumble as soon as I try to polish them with my cloth.\n
+Whatever are we to do?”`,
+      image: "fairy_3.png",
+    },
+    {
+      text: `“Yes.”said Canine “I know exactly what’s wrong,\n
+The children are beginning to eat far too many sweets\n
+and are not looking after their teeth at all.”\n 
+“I know…we can fix it!” Said Molar the wise one.\n
+She sounded quite excited by her grand idea.\n
+The three fairies looked at each other\n
+and waited while Molar explained all.\n
+As usual, it was a fabulous idea.\n
+The three of them wasted no time at all\n
+and got to work straight away.`,
+      image: "fairy_4.png",
+    },
+    {
+      text: `That night, it was Canine’s turn to fly off and collect\n
+Hall the milk teeth from under the sleeping children’s pillow.\n
+Off she flew with a bag of coins, some white berry sandwiches\n
+for her lunch and a pack of cards which\n
+the three fairies had spent all afternoon preparing.\n
+It was such a treat to leave the forest and see the city skyline\n
+but it was a dangerous place for fairies.\n
+Canine had to make sure that\n
+the big people never saw her.`,
+      image: "fairy_5.png",
+    },
+    {
+      text: `She flew through the starry sky\n
+and saw the wonderful city fast asleep.\n
+Her first stop was a young boy named Junior.\n
+Earlier in the night Junior and his mum had placed\n
+his milk tooth under the pillow.\n
+Mummy tucked Junior into bed and told him all about\n
+the tooth fairy who would leave some money in exchange for his tooth.\n
+She gave him a goodnight kiss and left him to fall asleep.`,
+      image: "fairy_6.png",
+    },
+
+    {
+      text: `By the time Canine arrived, Junior was fast asleep.\n
+She flew in through the top window that was ajar\n
+and glided quickly down to Junior’s pillow.\n
+Earlier in the night Junior and his mum had placed\n
+his milk tooth under the pillow.\n
+She perched beside him and braced\n
+herself in preparation for the task…`,
+      image: "fairy_7.png",
+    },
+
+    {
+      text: `“Heave……..” she lifted the pillow and\n
+wedged herself between the pillow and the bed.\n
+She quickly grabbed the tooth\n
+and tossed it over to her backpack.\n
+She placed two coins and the fairy’s special card\n
+in exchange under the pillow and flew off into\n
+the night sky to find more milk teeth.\n
+In the morning, as soon as Junior woke up,`,
+      image: "fairy_8.png",
+    },
+
+    {
+      text: `he rummaged under the pillow to see if\n
+the tooth fairy had left any money for him.\n
+He was thrilled to find the coins but looked blankly\n
+at the card he found in his hand.\n
+Mummy didn’t mention anything about a card…\n
+he wondered. He read the card out aloud…\n
+The Tooth Fairy’s guide to healthy teeth:\n
+Eat Healthy, cut down on sugary sweets & drinks,\n
+Brush your teeth twice a day, Use a fluoride toothpaste\n
+Visit the Dentist regularly.`,
+      image: "fairy_9.png",
+    },
+    {
+      text: `Back in the village of Ordon Gardens,\n
+the fairies were very happy about the new information card\n
+they were giving to the children.\n
+Soon enough the milk teeth coming into\n
+the factory were strong and healthy again.`,
+      image: "fairy_10.png",
+    },
+  ],
+};
+
+export default tales;
